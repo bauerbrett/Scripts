0c65935eaacda6826f1b8f9d1c28b0dca0b8afcd
bauerbrett <81916793+bauerbrett@users.noreply.github.com>
2023-08-17 14:26:17 -0400

    Update Az-Role-Assignment-Users.ps1

--- Az-Role-Assignment-Users.ps1
+++ Az-Role-Assignment-Users.ps1
@@ -1,10 +1,10 @@
-$Warriors = "50f1a1d8-806c-46ab-82f4-41fb54e730d8"
-$Timberwolves = "c46a6ca5-2236-4b9d-aff2-e465190d0b3b"
-$AzSub1 = "3b8667c6-8f75-42ea-b301-bf27c9db8674"
+$test = "50f1a1d8-806c-46ab-82f4-41fb54e730d8"
+$test2 = "c46a6ca5-2236-4b9d-aff2-e465190d0b3b"
+$test3 = "3b8667c6-8f75-42ea-b301-bf27c9db8674"
 
 $user = Read-Host "Enter the user principal name"
 
-$subscriptionIds = @($Warriors, $Timberwolves, $AzSub1)
+$subscriptionIds = @($test, $test2, $test3)
 
 
 $roleAssignments = foreach ($subscriptionId in $subscriptionIds) {
