c30e9db5ad6e399ea5290a2205c2504bb846ccf4
bauerbrett <81916793+bauerbrett@users.noreply.github.com>
2023-08-17 14:25:17 -0400

    Update Az-Role-Assignment-Subs.ps1

--- Az-Role-Assignment-Subs.ps1
+++ Az-Role-Assignment-Subs.ps1
@@ -1,9 +1,11 @@
+#Put your subscriptions in here
 $SubIDs = @(
     "50f1a1d8-806c-46ab-82f4-41fb54e730d8",
     "c46a6ca5-2236-4b9d-aff2-e465190d0b3b",
     "3b8667c6-8f75-42ea-b301-bf27c9db8674"
 )
 
+#Runs commands for each of these subscriptions
 $RoleAssignments = @()
 foreach ($subscriptionId in $SubIDs) {
     Set-AzContext -Subscription $subscriptionId
